f49486e76c9b41ce808cdb9cf42cbd1207fca3e0
Devesh Sharma <devesh.sharma050212@gmail.com>
2026-08-13 20:41:41 +0530

    added login route and controller function for user authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
+
 /*
   Controller function to handle user registration
   POST /api/auth/register
 */
+
 async function userRegisterController(req, res) {
     const { email, password, name } = req.body;
 
@@ -36,6 +38,41 @@ async function userRegisterController(req, res) {
     });
 }
 
+/*
+  Controller function to handle user login
+  POST /api/auth/login
+*/
+
+async function userLoginController(req, res) {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email: email }).select("+password");
+
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+    res.cookie('token', token);
+    res.status(200).json({
+        user: {
+            id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token: token,
+        message: 'User logged in successfully',
+        status: "Success",
+    });
+}
+
 module.exports = {
-    register: userRegisterController
+    register: userRegisterController,
+    login: userLoginController,
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller.js');
 
+/* POST /api/auth/register */
 router.post('/register', authController.register);
 
-router.get('/login', authController.login);
+/* POST /api/auth/login */
+router.post('/login', authController.login);
 
 module.exports = router;
